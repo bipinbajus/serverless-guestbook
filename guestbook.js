8553dc4c7b57aa4851a32e856564b4981926a1f6
bipinbajus <bipinbaj.us@gmail.com>
2021-12-09 17:57:32 +1100

    adding api

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://72562c94.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
